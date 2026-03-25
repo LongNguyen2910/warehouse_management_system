a2534a90e43d947ce91f42982b32ca22c16a311e
Wicky <giaphien1008@gmail.com>
2026-03-26 00:42:19 +0700

    add getAll Product

--- src/inventory_api.py
+++ src/inventory_api.py
@@ -1,2 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+import db_helper
 inventory_bp = Blueprint('inventory', __name__)
+
+@inventory_bp.route('/products', methods=['GET'])
+def get_products():
+    conn = db_helper.get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, sku, name FROM products")
+
+    rows = cursor.fetchall()
+    products = []
+    for row in rows:
+        products.append({
+            'id': row.id,
+            'sku': row.sku,
+            'name': row.name
+        })
+    conn.close()
+    return jsonify(products)
+
